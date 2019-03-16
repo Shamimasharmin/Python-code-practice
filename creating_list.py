d129d78bff93e7b319783aea43f2842602a9c10d
Shamima Sharmin <munmunshamima@gmail.com>
2019-03-16 13:30:04 +0600

    add creating_list.py

---
+++ creating_list.py
@@ -0,0 +1,14 @@
+list=[]
+for i in range(1,5):
+    list=[]
+    for j in range(1,i+1):
+        list.append(i*j)
+    print(list)
+    print("\n")
+list2=[]
+for i2 in range(3,0,-1):
+    list2=[]
+    for j2 in range(1,i2+1):
+        list2.append(i2*j2)
+    print(list2)
+    print("\n")
